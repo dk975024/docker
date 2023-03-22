2e0a25ae2300428d4ee4ac0a7137b1dd78ecb38c
dk975024 <43377507+dk975024@users.noreply.github.com>
2023-03-22 15:56:16 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -14,18 +14,18 @@ resource "aws_security_group" "allow_web" {
   name        = "allow_tls"
   description = "Allow web"
   vpc_id      = aws_vpc.main.id
- ingress {
-    description      = "web"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
- ingress {
-    description      = "ssh"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+  ingress {
+     description      = "web"
+     from_port        = 80
+     to_port          = 80
+     protocol         = "tcp"
+     cidr_blocks      = ["0.0.0.0/0"]
+   }
+  ingress {
+     description      = "ssh"
+     from_port        = 22
+     to_port          = 22
+     protocol         = "tcp"
+     cidr_blocks      = ["0.0.0.0/0"]
+   }
 }
