dac70f7ee8e5798bbc7390cd30d2c47c3328da1f
dk975024 <43377507+dk975024@users.noreply.github.com>
2023-03-22 15:41:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -12,10 +12,3 @@ resource "aws_security_group" "allow_web" {
   description = "Allow web"
   vpc_id      = aws_vpc.main.id
 }
-  ingress {
-    description      = "web"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
