5e739599629d1b5bccf4390445d65f4ae52f7f81
dk975024 <43377507+dk975024@users.noreply.github.com>
2023-03-22 15:31:49 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,5 +5,5 @@ resource "aws_instance" "web" {
   ami                     = "ami-0c9978668f8d55984"
   instance_type           = "t2.micro"
   key_name = "10march23"
-  tenancy                 = "host"
+  tenancy                 = "default"
 }
