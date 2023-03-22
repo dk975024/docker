c5fdb55508d388474006c045a4547bbf9550bab3
dk975024 <43377507+dk975024@users.noreply.github.com>
2023-03-22 15:21:04 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "this" {
+  ami                     = "ami-0c9978668f8d55984"
+  instance_type           = "t2micro"
+  key = "10march23"
+  tenancy                 = "host"
+}
