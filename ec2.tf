7a31b4b145a2b3bf3ab62741e2f272670083bf52
dk975024 <43377507+dk975024@users.noreply.github.com>
2023-03-22 15:36:59 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,3 +7,15 @@ resource "aws_instance" "web" {
   key_name = "10march23"
   tenancy                 = "default"
 }
+resource "aws_security_group" "allow_web" {
+  name        = "allow_tls"
+  description = "Allow web"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "web"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [0.0.0.0/0]
+  }
