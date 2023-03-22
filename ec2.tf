0444414062895be98c613ce1259f66b23b70c3f9
dk975024 <43377507+dk975024@users.noreply.github.com>
2023-03-22 15:24:40 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "this" {
+resource "aws_instance" "web" {
   ami                     = "ami-0c9978668f8d55984"
-  instance_type           = "t2micro"
+  instance_type           = "t2.micro"
   key_name = "10march23"
   tenancy                 = "host"
 }
